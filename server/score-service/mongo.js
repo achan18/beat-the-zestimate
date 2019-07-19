44897b386f2382d74e36bdb04b6d76fc40fcb042
Chris <wonl3@uci.edu>
2019-07-19 16:44:25 -0700

    Beat the Zestimate Score Service

--- server/score-service/mongo.js
+++ server/score-service/mongo.js
@@ -19,19 +19,15 @@ function getLogError(parcelid) {
       assert.equal(null, err);
       console.log('Connected successfully to server');
 
-      // hard coded parselid
-      //    var parselid = 17177301;
       const db = client.db(dbName);
       const collection = db.collection('sales');
-      let query = { "parcelid": 17177301 };
+      let query = { "parcelid": parcelid };
 
       collection.find(query).toArray(function (err, docs) {
         assert.equal(err, null);
         logerror = docs[0]['logerror'];
         resolve(logerror);
       });
-
-      client.close();
     });
   });
 
@@ -45,74 +41,33 @@ function getLeaderBoard(parcelid) {
       const collection = db.collection('scores');
       let mySort = { score: -1 };
       let query = { "parcelid": parcelid };
-      //dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
       collection.find(query).sort(mySort).toArray(function (error, result) {
         console.log(result);
         resolve(result);
       });
     });
 
-    client.close();
+    //    client.close();
   });
 
   return p;
 }
 
-function insertScore(name, userZestimate, parcelid, score) {
-  /*   var p = new Promise((resolve, reject) => {
-  
-      client.connect(function (err) {
-        assert.equal(null, err);
-        console.log('Connected successfully to server');
-        const db = client.db(dbName);
-        const collection = db.collection('scores');
-  
-        collection.insertOne({
-          name: name,
-          userZestimate: userZestimate,
-          parcelid: parcelid,
-          score: score,
-          createDate: Date.now()
-        }, function (err, res) {
-          console.log('result:', res);
-          if (err) console.log(err);
-          resolve(res);
-        });
-  
-        client.close();
-  
-      });
-    });
-  
-    return p; */
-  var p = new Promise((resolve, reject) => {
-    var logerror;
-    client.connect(function (err) {
-      assert.equal(null, err);
-      console.log('Connected successfully to server');
+async function insertScore(name, userZestimate, parcelid, score) {
 
-      const db = client.db(dbName);
-      const collection = db.collection('scores');
+  const connection = await MongoClient.connect(url, { useNewUrlParser: true });
+  console.log('Connected successfully to server');
+  const db = connection.db(dbName);
+  const collection = db.collection('scores');
 
-      collection.insertOne({
-        name: name,
-        userZestimate: userZestimate,
-        parcelid: parcelid,
-        score: score,
-        createDate: Date.now()
-      }, function (err, result) {
-        assert.equal(err, null);
-        resolve(result);
-      });
-
-      client.close();
-    });
+  await collection.insertOne({
+    name: name,
+    userZestimate: userZestimate,
+    parcelid: parcelid,
+    score: score,
+    createDate: Date.now()
   });
-
-  return p;
 }
-
-//insertScore('chris', 1000000, 142214, 1000);
 
 module.exports.getLogError = getLogError;
 module.exports.getLeaderBoard = getLeaderBoard;
--- server/score-service/zestimate.js
+++ server/score-service/zestimate.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const parser = require('xml2js').parseString;
 
 async function getZestimate(zpid) {
-    var resp = await axios.get('http://www.zillow.com/webservice/GetZestimate.htm?zws-id=X1-ZWz17rpytqj9jf_6le3g&zpid=48749425');
+    var resp = await axios.get(`http://www.zillow.com/webservice/GetZestimate.htm?zws-id=X1-ZWz17rpytqj9jf_6le3g&zpid=${zpid}`);
     return resp.data;
 }
 
