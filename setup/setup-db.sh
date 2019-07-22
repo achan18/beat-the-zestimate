3d7ce4419f62c7b9838f7ac948a0a5c25aa44ec0
achan18 <austinch@zillowgroup.com>
2019-07-21 22:03:59 -0700

    implemented new endpoint for leaderboard

--- setup/setup-db.sh
+++ setup/setup-db.sh
@@ -1,2 +1,3 @@
 mongoimport -d hackweek -c property --type csv --file server/property-service/data/properties_2016.csv --headerline
 mongoimport -d hackweek -c sales --type csv --file server/score-service/data/train_2016_v2.csv --headerline
+mongo --eval "db.scores.deleteMany({})" hackweek
